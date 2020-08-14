3ff00eb4e1e768888620fcdfa5b33be07243d19b
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-08-14 19:04:25 +0700

    revisi view login & register

---
+++ resources/views/layouts/bg.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Sanbercode</title>
+
+    <link href="{{ asset('sbadmin2/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Pacifico" />
+
+    <!-- Styles -->
+    <style>
+        #welcome {
+            background: url("{{ asset('img/welcome.jpg') }}");
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+        .container-fluid {
+            background: rgba(42, 38, 61, 0.82);
+            height: 100vh;
+        }
+
+    </style>
+    @stack('style')
+</head>
+
+<body id="welcome">
+    <div class="container-fluid">
+        @yield('content')
+    </div>
+
+    @stack('script')
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.bg')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Sanbercode</title>
-
-    <link href="{{ asset('sbadmin2/css/sb-admin-2.min.css') }}" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Pacifico" />
-
-    <!-- Styles -->
+@push('style')
     <style>
-        #welcome {
-            background: url("{{ asset('img/welcome.jpg') }}");
-            background-repeat: no-repeat;
-            background-size: cover;
-        }
-
-        .container-fluid {
-            background: rgba(42, 38, 61, 0.82);
-            height: 100vh;
-        }
-
         h1 {
             margin-top: 200px;
             color: whitesmoke;
@@ -31,42 +10,38 @@
             line-height: 84px;
         }
 
-        li, a {
+        li,
+        a {
             color: whitesmoke;
         }
 
     </style>
-</head>
+@endpush
 
-<body id="welcome">
+@section('content')
+@if(Route::has('login'))
+    <ul class="nav justify-content-end">
 
-    <div class="container-fluid">
-        @if(Route::has('login'))
-            <ul class="nav justify-content-end">
-
-                @auth
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/home') }}">Home</a>
-                    </li>
-                @else
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">Login</a>
-                    </li>
-                    @if(Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">Register</a>
-                        </li>
-                    @endif
-                @endauth
-            </ul>
-        @endif
-        <div class="d-flex justify-content-center">
-            <h1>Sanbercode</h1>
-        </div>
-        <div class="d-flex justify-content-center">
-            <h5 style="color: whitesmoke">Feel free to ask..</h5>
-        </div>
-    </div>
-</body>
-
-</html>
+        @auth
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/home') }}">Home</a>
+            </li>
+        @else
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">Login</a>
+            </li>
+            @if(Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">Register</a>
+                </li>
+            @endif
+        @endauth
+    </ul>
+@endif
+<div class="d-flex justify-content-center">
+    <h1>Sanbercode</h1>
+</div>
+<div class="d-flex justify-content-center">
+    <h5 style="color: whitesmoke">Feel free to ask..</h5>
+</div>
+@endsection
